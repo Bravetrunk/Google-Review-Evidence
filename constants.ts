2e61a80dffa4d53c7c29ba7d2c53d22db3394964
Bravetrunk <133188045+Bravetrunk@users.noreply.github.com>
2025-11-01 12:26:05 +0700

    Delete constants.ts

--- constants.ts
+++
@@ -1,6 +0,0 @@
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx2MnvKWkZofBcwUg0KV9eRpdwEkj5WqJT4jB2VVoi_DEpo12MxqkgsLV_mvSbz94aa/exec';
-
-export const EMPLOYEE_OPTIONS = [
-    { value: "น้ำฝน", label: "น้ำฝน" },
-    { value: "ก้อย", label: "ก้อย" },
-];
